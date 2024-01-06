216c76a8f795f4414806df2e7a9780dec64f6429
Hyunsu <hs9880@gmail.com>
2024-01-06 20:59:49 +0900

    [ADD] 초기접근방식문제점

--- Hyunsu/set/섬연결하기.js
+++ Hyunsu/set/섬연결하기.js
@@ -2,6 +2,9 @@ const assert = require("assert");
 
 /**
  * 잘못된 접근 방식
+ * MST를 보고 난 후 접근의 문제점.
+- 1. 정렬을 하지 않아 모든 노드의 find를 찾기 위해 거치는 가중치의 합을 구하는 부분에서 사이클핸들링이 어려웠음. - 이 부분은 가중치가 작은 순서대로 정렬로 접근.
+- 2. 모든 엣지가 연결되어 있다는 점을 어느 시점에 알 수 있는지에 대한 의문을 해결하지 못했다. 이 부분은 양방향 인접리스트가 아닌 에지 리스트 형태로 유지해야 했었고 (사이클 핸들링이 쉬워짐), 모든 엣지가 연결되어 있다는 점은 트리와 그래프의 차이인 그래프 간선의 개수는 노드 개수보다 -1 개인점을 활용하여 해결.
  * @param {*} n
  * @param {*} costs
  * @returns
@@ -71,7 +74,6 @@ function union(current, root, disjoint) {
 }
 
 //✅ 크루스칼 알고리즘 반영
-
 function solution1(n, costs) {
   var answer = 0;
   const disjoint = Array.from({ length: n }, (_, i) => i);
