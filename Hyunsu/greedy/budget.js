33250616a65c20a52c4a65b623181379964798cb
Hyunsu <hs9880@gmail.com>
2024-02-29 23:12:20 +0900

    remove

--- Hyunsu/greedy/budget.js
+++ Hyunsu/greedy/budget.js
@@ -1,5 +1,4 @@
 // Url: https://programmers.co.kr/learn/courses/30/lessons/12982
-//  Date: 2021-07-07 Wed
 // 부서별로 신청한 금액이 들어있는 배열 d와 예산 budget이 매개변수로 주어질 때,
 // 최대 몇 개의 부서에 물품을 지원할 수 있는지 return 하도록 solution 함수를 완성해주세요.
 // 최대 많은 부서에 물품 지원  -> 적게 요청한 예산 순으로 정렬 -> 예산이 남아있는 동안 요청한 예산을 더함
